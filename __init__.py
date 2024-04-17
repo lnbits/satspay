39a1b20846a46ece2ec7e61f23022800dcffa472
dni ⚡ <office@dnilabs.com>
2024-04-17 22:34:03 +0200

    fix: properly start/stop tasks
    https://github.com/lnbits/lnbits/issues/2411

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
 import asyncio
-from typing import List
+from loguru import logger
 
 from fastapi import APIRouter
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
+from lnbits.tasks import create_permanent_unique_task
 
 db = Database("ext_satspay")
-
-scheduled_tasks: List[asyncio.Task] = []
 
 satspay_ext: APIRouter = APIRouter(prefix="/satspay", tags=["satspay"])
 
@@ -30,7 +28,15 @@ from .views import *  # noqa: F401,F403
 from .views_api import *  # noqa: F401,F403
 
 
+scheduled_tasks: list[asyncio.Task] = []
+
+def satspay_stop():
+    for task in scheduled_tasks:
+        try:
+            task.cancel()
+        except Exception as ex:
+            logger.warning(ex)
+
 def satspay_start():
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    task = create_permanent_unique_task("ext_satspay", wait_for_paid_invoices)
     scheduled_tasks.append(task)
