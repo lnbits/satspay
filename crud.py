36f2d43f9db2c917b62c386633aa4ef74951af35
dni ⚡ <office@dnilabs.com>
2024-10-18 09:28:13 +0200

    rc5

--- crud.py
+++ crud.py
@@ -56,21 +56,21 @@ async def create_charge(
             raise Exception(f"Wallet '{data.onchainwallet}' can no longer be accessed.")
 
     if data.lnbitswallet:
-        payment_hash, payment_request = await create_invoice(
+        payment = await create_invoice(
             wallet_id=data.lnbitswallet,
             amount=data.amount,
             memo=data.description,
             extra={"tag": "charge", "charge": charge.id},
             expiry=int(data.time * 60),
         )
-        charge.payment_hash = payment_hash
-        charge.payment_request = payment_request
-    await db.insert("satspay.charges", charge)  # type: ignore
+        charge.payment_hash = payment.payment_hash
+        charge.payment_request = payment.bolt11
+    await db.insert("satspay.charges", charge)
     return charge
 
 
 async def update_charge(charge: Charge) -> Charge:
-    await db.update("satspay.charges", charge)  # type: ignore
+    await db.update("satspay.charges", charge)
     return charge
 
 
@@ -78,14 +78,14 @@ async def get_charge(charge_id: str) -> Optional[Charge]:
     return await db.fetchone(
         "SELECT * FROM satspay.charges WHERE id = :id",
         {"id": charge_id},
-        Charge,  # type: ignore
+        Charge,
     )
 
 
 async def get_pending_charges() -> list[Charge]:
     return await db.fetchall(
         "SELECT * FROM satspay.charges WHERE paid = false",
-        model=Charge,  # type: ignore
+        model=Charge,
     )
 
 
@@ -93,7 +93,7 @@ async def get_charge_by_onchain_address(onchain_address: str) -> Optional[Charge
     return await db.fetchone(
         "SELECT * FROM satspay.charges WHERE onchainaddress = :address",
         {"address": onchain_address},
-        Charge,  # type: ignore
+        Charge,
     )
 
 
@@ -103,7 +103,7 @@ async def get_charges(user: str) -> list[Charge]:
         SELECT * FROM satspay.charges WHERE "user" = :user ORDER BY "timestamp" DESC
         """,
         {"user": user},
-        Charge,  # type: ignore
+        Charge,
     )
 
 
@@ -118,12 +118,12 @@ async def create_theme(data: CreateSatsPayTheme, user_id: str) -> SatsPayTheme:
         custom_css=data.custom_css,
         user=user_id,
     )
-    await db.insert("satspay.themes", theme)  # type: ignore
+    await db.insert("satspay.themes", theme)
     return theme
 
 
 async def update_theme(theme: SatsPayTheme) -> SatsPayTheme:
-    await db.update("satspay.themes", theme, "css_id = :css_id")  # type: ignore
+    await db.update("satspay.themes", theme, "css_id = :css_id")
     return theme
 
 
@@ -131,7 +131,7 @@ async def get_theme(css_id: str) -> Optional[SatsPayTheme]:
     return await db.fetchone(
         "SELECT * FROM satspay.themes WHERE css_id = :css_id",
         {"css_id": css_id},
-        SatsPayTheme,  # type: ignore
+        SatsPayTheme,
     )
 
 
@@ -141,7 +141,7 @@ async def get_themes(user_id: str) -> list[SatsPayTheme]:
         SELECT * FROM satspay.themes WHERE "user" = :user ORDER BY title DESC
         """,
         {"user": user_id},
-        SatsPayTheme,  # type: ignore
+        SatsPayTheme,
     )
 
 
@@ -154,19 +154,19 @@ async def delete_theme(theme_id: str) -> None:
 async def get_or_create_satspay_settings() -> SatspaySettings:
     settings = await db.fetchone(
         "SELECT * FROM satspay.settings LIMIT 1",
-        model=SatspaySettings,  # type: ignore
+        model=SatspaySettings,
     )
     if settings:
         return settings
     else:
         settings = SatspaySettings()
-        await db.insert("satspay.settings", settings)  # type: ignore
+        await db.insert("satspay.settings", settings)
         return settings
 
 
 async def update_satspay_settings(settings: SatspaySettings) -> SatspaySettings:
     # 3rd arguments `WHERE clause` is empty for settings
-    await db.update("satspay.settings", settings, "")  # type: ignore
+    await db.update("satspay.settings", settings, "")
     return settings
 
 
