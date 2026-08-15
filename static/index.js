lnbits/satspay

commit c424acfc7cd833d719b6eb3a3739dee62a8a3353

fix: use g.user.fiat_providers instead of separate API call; add fiat provider dropdown

static/index.js
@@ -138,7 +138,6 @@ window.PageSatspay = {
   data() {
     return {
       fiatRates: {},
-      fiatProviders: [],
       settings: [
         {
           type: 'str',
@@ -468,38 +467,3 @@ window.PageSatspay = {
         })
         .catch(LNbits.utils.notifyApiError)
     },
-    onFiatToggle(val) {
-      if (val && this.fiatProviders.length > 0) {
-        this.formDialogCharge.data.fiat_provider = this.fiatProviders[0]
-      } else {
-        this.formDialogCharge.data.fiat_provider = ''
-      }
-    },
-    loadFiatProviders: async function () {
-      try {
-        const {data} = await LNbits.api.request(
-          'GET',
-          '/satspay/api/v1/fiat/providers',
-          this.g.user.wallets[0].inkey
-        )
-        this.fiatProviders = data || []
-      } catch (error) {
-        this.fiatProviders = []
-      }
-    }
-  },
-  async created() {
-    try {
-      const {data} = await LNbits.api.request(
-        'GET',
-        '/satspay/api/v1/settings/public'
-      )
-      this.network = data.network
-    } catch (e) {}
-    if (this.g.user.admin) {
-      await this.getThemes()
-    }
-    await this.getCharges()
-    await this.getWalletLinks()
-  }
-}

static/index.vue
@@ -486,56 +486,14 @@
             :label="$t('satspay.mins_valid')"
           ></q-input>
 
-          <div class="row">
-            <div class="col">
-              <div v-if="walletLinks.length > 0">
-                <q-checkbox
-                  v-model="formDialogCharge.data.onchain"
-                  :label="$t('satspay.onchain')"
-                ></q-checkbox>
-              </div>
-              <div v-else>
-                <q-checkbox
-                  :value="false"
-                  :label="$t('satspay.onchain')"
-                  disabled
-                >
-                  <q-tooltip
-                    v-text="$t('satspay.onchain_wallet_tooltip')"
-                  ></q-tooltip>
-                </q-checkbox>
-              </div>
-            </div>
-            <div class="col">
-              <q-checkbox
-                v-model="formDialogCharge.data.lnbits"
-                :label="$t('satspay.lnbits_wallet_label')"
-              ></q-checkbox>
-            </div>
-            <div class="col">
-              <div v-if="fiatProviders.length > 0">
-                <q-checkbox
-                  v-model="formDialogCharge.data.fiat"
-                  :label="$t('satspay.fiat_payment')"
-                  @update:model-value="onFiatToggle"
-                ></q-checkbox>
-              </div>
-              <div v-else>
-                <q-checkbox
-                  :value="false"
-                  :label="$t('satspay.fiat_payment')"
-                  disabled
-                >
-                  <q-tooltip
-                    v-text="$t('satspay.fiat_not_configured')"
-                  ></q-tooltip>
-                </q-checkbox>
-              </div>
-            </div>
-          </div>
-
-          <div v-if="formDialogCharge.data.onchain">
+          <div v-if="walletLinks.length > 0">
+            <q-checkbox
+              v-model="formDialogCharge.data.onchain"
+              :label="$t('satspay.onchain')"
+            ></q-checkbox>
             <q-select
+              v-if="formDialogCharge.data.onchain"
+              class="q-mt-sm"
               filled
               dense
               emit-value
@@ -546,7 +504,7 @@
             <q-item
               tag="label"
               v-ripple
-              v-if="!formDialogCharge.data.fasttrack"
+              v-if="formDialogCharge.data.onchain && !formDialogCharge.data.fasttrack"
             >
               <q-item-section avatar top>
                 <q-checkbox
@@ -563,7 +521,7 @@
                 ></q-item-label>
               </q-item-section>
             </q-item>
-            <q-item tag="label" v-ripple v-if="!formDialogCharge.data.zeroconf">
+            <q-item tag="label" v-ripple v-if="formDialogCharge.data.onchain && !formDialogCharge.data.zeroconf">
               <q-item-section avatar top>
                 <q-checkbox
                   v-model="formDialogCharge.data.fasttrack"
@@ -580,9 +538,25 @@
               </q-item-section>
             </q-item>
           </div>
+          <div v-else>
+            <q-checkbox
+              :value="false"
+              :label="$t('satspay.onchain')"
+              disabled
+            >
+              <q-tooltip
+                v-text="$t('satspay.onchain_wallet_tooltip')"
+              ></q-tooltip>
+            </q-checkbox>
+          </div>
 
+          <q-checkbox
+            v-model="formDialogCharge.data.lnbits"
+            :label="$t('satspay.lnbits_wallet_label')"
+          ></q-checkbox>
           <q-select
             v-if="formDialogCharge.data.lnbits"
+            class="q-mt-sm"
             filled
             dense
             emit-value
@@ -590,6 +564,34 @@
             :options="g.user.walletOptions"
             :label="$t('satspay.wallet')"
           ></q-select>
+
+          <div v-if="g.user.fiat_providers && g.user.fiat_providers.length">
+            <q-checkbox
+              v-model="formDialogCharge.data.fiat"
+              :label="$t('satspay.fiat_payment')"
+            ></q-checkbox>
+            <q-select
+              v-if="formDialogCharge.data.fiat"
+              class="q-mt-sm"
+              filled
+              dense
+              emit-value
+              v-model="formDialogCharge.data.fiat_provider"
+              :options="g.user.fiat_providers"
+              :label="$t('satspay.fiat_payment')"
+            ></q-select>
+          </div>
+          <div v-else>
+            <q-checkbox
+              :value="false"
+              :label="$t('satspay.fiat_payment')"
+              disabled
+            >
+              <q-tooltip
+                v-text="$t('satspay.fiat_not_configured')"
+              ></q-tooltip>
+            </q-checkbox>
+          </div>
 
           <q-toggle
             v-model="showAdvanced"

commit af1f82cb2327192d2091945b178f59c4ee5db78f

fix: restore truncated index.js ending (closes methods, restores created hook)
Commit c424acf accidentally removed the closing braces and the created()
hook along with the fiat provider methods, leaving the file with a
syntax error so the SatsPay page never rendered in the browser.

static/index.js
@@ -466,4 +466,20 @@ window.PageSatspay = {
           this.fiatRates = rates
         })
         .catch(LNbits.utils.notifyApiError)
-    },
+    }
+  },
+  async created() {
+    try {
+      const {data} = await LNbits.api.request(
+        'GET',
+        '/satspay/api/v1/settings/public'
+      )
+      this.network = data.network
+    } catch (e) {}
+    if (this.g.user.admin) {
+      await this.getThemes()
+    }
+    await this.getCharges()
+    await this.getWalletLinks()
+  }
+}